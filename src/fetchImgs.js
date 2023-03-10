70a6fb0b90be6a9b16f52e402bd04631edacc0ed
GGalina <71042425+GGalina@users.noreply.github.com>
2023-03-10 14:41:35 +0400

    HW

---
+++ src/fetchImgs.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default class fetchImgs {
+    constructor() {
+        this.query = '';
+        this.page = 1;
+    }
+    async fetchData() {
+        const BASE_URL = 'https://pixabay.com/api/';
+        const API_KEY = '34272831-3ed6bcfb2b9ae97f1edcdbcde';
+        
+        try {
+            const response = await axios.get(
+                `${BASE_URL}?key=${API_KEY}&q=${this.query}&per_page=15&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`);
+            this.page += 1;
+            return response.data;
+        } catch (error) {
+        console.log(error);
+        }
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,54 @@
+import './css/styles.css';
+import fetchImgs from './fetchImgs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { displaySearchList, slider} from './displayResults';
+
+export const { inputEl, submitBtnEl, galleryEl, loadMoreBtnEl } = {
+    inputEl: document.querySelector(`#search-form`),
+    submitBtnEl: document.querySelector(`[type="submit"]`),
+    galleryEl: document.querySelector(`.gallery`),
+    loadMoreBtnEl: document.querySelector('.load-more')
+};
+
+const searchImg = new fetchImgs();
+
+inputEl.addEventListener(`submit`, onSearch);
+loadMoreBtnEl.addEventListener(`click`, onLoadMore);
+
+async function onSearch(event) {
+    event.preventDefault();
+    loadMoreBtnEl.classList.add('is-hidden');
+    galleryEl.innerHTML = '';
+    searchImg.query = event.currentTarget.elements['searchQuery'].value;
+
+    try {
+        const returnedData = await searchImg.fetchData();
+        if (returnedData.hits.length === 0) {
+            Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            return;
+        } else {
+            Notify.success(`Hooray! We found ${returnedData.totalHits} images.`);
+            displaySearchList(returnedData.hits);
+            loadMoreBtnEl.classList.remove('is-hidden');
+        }
+    }
+    catch (error) {
+            console.log(error)
+        }
+};
+
+async function onLoadMore(event) {
+    event.preventDefault();
+    try {
+        const returnedData = await searchImg.fetchData();
+        displaySearchList(returnedData.hits);
+        slider.refresh();
+      
+        if (galleryEl.children.length === returnedData.totalHits) {
+            loadMoreBtnEl.classList.add('is-hidden');
+            Notify.info("We're sorry, but you've reached the end of search results.");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
